d6e3aba51f4f6fc5b5c36a6a927416fcb50813c1
HaiDM <hai-4181@NO-002395.ad-wbcdom.wbc.co.jp>
2014-10-01 13:57:30 +0900

    copyright

--- SlugHelper.php
+++ SlugHelper.php
@@ -1,4 +1,28 @@
 <?php
+/**
+ *	The MIT License (MIT)
+ *
+ *	Copyright (c) 2014 CoderGarden
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy
+ *	of this software and associated documentation files (the "Software"), to deal
+ *	in the Software without restriction, including without limitation the rights
+ *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *	copies of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in all
+ *	copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *	SOFTWARE.
+ */
+
 class SlugHelper {
 
 	/* 
@@ -7,8 +31,10 @@ class SlugHelper {
 	*/
 	private $separator = "-";
 	/* 
-	 * Max length for slug in order to guarantee our slug' length is not over slug field's length in database. 
-	 * Giới hạn độ dài cho slug nhằm tránh vấn đề độ dài slug vượt quá độ dài quy định trong cơ sở dữ liệu.
+	 * Max length for slug in order to guarantee our slug' length 
+	 * is not over slug field's length in database. 
+	 * Giới hạn độ dài cho slug nhằm tránh vấn đề độ dài slug 
+	 * vượt quá độ dài quy định trong cơ sở dữ liệu.
 	*/
     private $max_length = null;
 	/* 
